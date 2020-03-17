0ced74f5c1627991a80fa0a5197f3d80e6f4ab09
Jardel Kuhn <jardelkuhn@gmail.com>
2020-03-17 18:27:51 -0300

    chore: added signIn prototype screen

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import { View, Text, StatusBar } from 'react-native';
 
 import theme from './global/theme'
 
 import Routes from './routes'
+import SignIn from './screens/SignIn'
+const Stack = createStackNavigator();
 
 export default function App() {
+    const authenticated = false;
 
     return (
         <NavigationContainer>
-            <Routes />
+            <Stack.Navigator>
+                <Stack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }} />
+                <Stack.Screen name="Routes" component={Routes} options={{ headerShown: false }}/>
+            </Stack.Navigator>
             <StatusBar barStyle="light-content" backgroundColor={theme.color.primaryDarkColor} />
         </NavigationContainer>
     );
--- mobile/src/screens/SignIn/index.js
+++ mobile/src/screens/SignIn/index.js
@@ -1,13 +1,61 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Image, TouchableOpacity } from 'react-native';
 
-export default function SignIn() {
+import {
+    Container,
+    FormInput,
+    Form,
+    SubmitButton,
+} from './styles';
+
+import { MyLoginBackground } from './styles';
+//import { signInRequest } from '../../store/modules/auth/actions';
+
+export default function SignIn({ navigation }) {
+    const [user, setUser] = useState();
+    const [password, setPassword] = useState();
+    const passwordRef = useRef();
+
+    const loading = false;//useSelector(state => state.auth.loading);
+
+    function handleSubmit() {
+        navigation.navigate('Routes');
+    }
 
     return (
-        <View>
-            <Text>
-            SignIn
-            </Text>
-        </View>
+        <MyLoginBackground>
+            <Container>
+                <Form>
+                    <TouchableOpacity>
+                        <FormInput
+                            icon="mail-outline"
+                            keyboardType="email-address"
+                            autoCorrect={false}
+                            autoCapitalize="words"
+                            placeholder="Usuário"
+                            returnKeyType="next"
+                            value={user}
+                            onChangeText={setUser}
+                            onSubmitEditing={() => passwordRef.current.focus()}
+                        />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity>
+                        <FormInput
+                            icon="lock-outline"
+                            secureTextEntry
+                            placeholder="Senha"
+                            ref={passwordRef}
+                            returnKeyType="send"
+                            value={password}
+                            onChangeText={setPassword}
+                            onSubmitEditing={handleSubmit}
+                        />
+                    </TouchableOpacity>
+
+                    <SubmitButton loading={loading} onPress={handleSubmit}>Acessar</SubmitButton>
+                </Form>
+            </Container>
+        </MyLoginBackground>
     );
 }
